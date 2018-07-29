28b8d49bd8185c658d7970ca1524fc1abc2e3a61
Andrés Obando Alfaro <31627028+Andres49815@users.noreply.github.com>
2018-07-29 16:43:23 -0600

    Index de Persona y de Pais

---
+++ CountriesApp/CountriesApp/Controllers/CountriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CountriesApp.Models;
+
+namespace CountriesApp.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private CountriesEntities db = new CountriesEntities();
+
+        // GET: Countries
+        public ActionResult Index()
+        {
+            var countries = db.Countries.Include(c => c.Person);
+            return View(countries.ToList());
+        }
+
+        // GET: Countries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Create
+        public ActionResult Create()
+        {
+            ViewBag.presidentID = new SelectList(db.People, "id", "firstName");
+            return View();
+        }
+
+        // POST: Countries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,area,population,flag,anthem,presidentID")] Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Countries.Add(country);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.presidentID = new SelectList(db.People, "id", "firstName", country.presidentID);
+            return View(country);
+        }
+
+        // GET: Countries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.presidentID = new SelectList(db.People, "id", "firstName", country.presidentID);
+            return View(country);
+        }
+
+        // POST: Countries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,area,population,flag,anthem,presidentID")] Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(country).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.presidentID = new SelectList(db.People, "id", "firstName", country.presidentID);
+            return View(country);
+        }
+
+        // GET: Countries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Country country = db.Countries.Find(id);
+            db.Countries.Remove(country);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CountriesApp/CountriesApp/Controllers/PeopleController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CountriesApp.Models;
+
+namespace CountriesApp.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private CountriesEntities db = new CountriesEntities();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            var people = db.People.Include(p => p.Country).Include(p => p.Country1);
+            return View(people.ToList());
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            ViewBag.birthCountry = new SelectList(db.Countries, "id", "name");
+            ViewBag.residenceCountry = new SelectList(db.Countries, "id", "name");
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,idNumber,firstName,lastName,birthCountry,residenceCountry,birthdate,email,photo,interview")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.birthCountry = new SelectList(db.Countries, "id", "name", person.birthCountry);
+            ViewBag.residenceCountry = new SelectList(db.Countries, "id", "name", person.residenceCountry);
+            return View(person);
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.birthCountry = new SelectList(db.Countries, "id", "name", person.birthCountry);
+            ViewBag.residenceCountry = new SelectList(db.Countries, "id", "name", person.residenceCountry);
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,idNumber,firstName,lastName,birthCountry,residenceCountry,birthdate,email,photo,interview")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.birthCountry = new SelectList(db.Countries, "id", "name", person.birthCountry);
+            ViewBag.residenceCountry = new SelectList(db.Countries, "id", "name", person.residenceCountry);
+            return View(person);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Person person = db.People.Find(id);
+            db.People.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
