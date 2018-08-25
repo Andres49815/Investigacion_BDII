6cc0267a0910b07b99512fd1240ce89a0299e8bb
Andrés Obando Alfaro <31627028+Andres49815@users.noreply.github.com>
2018-08-25 10:17:47 -0600

    Consulta 2

---
+++ CountriesApp/CountriesApp/Models/TotalPeople_Q.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountriesApp.Models
+{
+    public class TotalPeople_Q
+    {
+        public int BirthYear { get; set; }
+        public string CountryName { get; set; }
+        public int PeopleBorned { get; set; }
+        public bool IsTotal { get; set; }
+
+        public TotalPeople_Q(int BirthYear = -1, string CountryName = "", int PeopleBorned = 0)
+        {
+            this.BirthYear = BirthYear;
+            this.CountryName = CountryName;
+            this.PeopleBorned = PeopleBorned;
+            this.IsTotal = CountryName.Equals("");
+        }
+    }
+}
